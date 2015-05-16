bfcc5baf8ce4ee7883bd737ca93d63a77c895cb9
Shea Bunge <info@bungeshea.com>
2015-05-16 18:21:11 +1000

    Rename LCDUtils class and move HelloWorld class from test package

---
+++ shea/ev3/HelloWorld.java
@@ -0,0 +1,33 @@
+package shea.ev3;
+
+import lejos.hardware.Battery;
+import lejos.hardware.Sound;
+import lejos.utility.Delay;
+
+/**
+ * This is a basic leJOS program
+ * @author Shea Bunge
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * Main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String message = args.length > 0 ? args[0] : "Hello world!";
+
+		System.out.println(message);
+
+		Delay.msDelay(500);
+		Sound.beep();
+
+		System.out.println();
+		System.out.println("Battery voltage:");
+		System.out.println(Battery.getVoltage());
+
+		Delay.msDelay(3000);
+	}
+
+}
